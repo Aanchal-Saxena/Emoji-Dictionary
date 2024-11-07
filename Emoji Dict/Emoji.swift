e8018388a85e90a9897e3a8054bf1a676d848bdd
Aanchal-wmp <aanchalsaxena@wingmanpartners.com>
2024-11-07 12:24:10 +0530

    first commit

---
+++ Emoji Dict/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  Emoji Dict
+//
+//  Created by Batch-2 on 01/05/24.
+//
+
+import Foundation
+struct Emoji
+{
+    var symbol: String
+    var name: String
+    var description: String
+    var uasge: String
+}
---
+++ Emoji Dict/EmojiTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiTableViewCell.swift
+//  Emoji Dict
+//
+//  Created by Batch-2 on 01/05/24.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    
+    @IBOutlet var symbolLabel: UILabel!
+    
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(with emoji: Emoji)
+    {
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+    }
+
+}
